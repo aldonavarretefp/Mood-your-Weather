a6533fb8d04325a7657c1cb0681adb9d40f4880b
Aldo Navarrete <aldo.navarrete@fi.unam.edu>
2023-10-19 16:08:01 +0200

    Changed Large and Inline navigation color titles

--- MoodYourWeather/MoodYourWeatherApp.swift
+++ MoodYourWeather/MoodYourWeatherApp.swift
@@ -10,6 +10,12 @@ import SwiftData
 
 @main
 struct MoodYourWeatherApp: App {
+    
+    init() {
+        // Large and inline navigation title appereance.
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.accent]
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.accent]
+    }
 
     var body: some Scene {
         WindowGroup {
